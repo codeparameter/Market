4f5de9acb5d3a8c2b17b32c1d9dfe165dbdbdfe1
codeparameter <codeparameter.com@gmail.com>
2023-10-31 19:35:44 +0330

    starting pages

--- src/js/ethers.js
+++ src/js/ethers.js
@@ -1,4 +1,4 @@
-import { ethers } from "../node_modules/ethers/dist/ethers.esm.min.js";
+import { ethers } from "../../node_modules/ethers/dist/ethers.esm.min.js";
 
 // A Web3Provider wraps a standard Web3 provider, which is
 // what MetaMask injects as window.ethereum into each page
