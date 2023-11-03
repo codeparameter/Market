6dc0de970aaea1224dac310b7da9ce0f5f3eaa9d
ArshiaZarei <arshiazarei8@gmail.com>
2023-11-03 17:38:05 +0330

    add token form and request list components

---
+++ src/components/TokenForm.jsx
@@ -0,0 +1,39 @@
+import { chakra, Checkbox, Heading, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import CustomNumberInput from "./common/CustomNumberInput";
+
+const TokenForm = () => {
+	const [tokenAmount, setTokenAmount] = useState(0);
+	const [weiAmount, setWeiAmount] = useState(0);
+	const [allAtOnceChecked, setAllAtOnceChecked] = useState(true);
+	const [formState, setFormState] = useState(true);
+
+	const toggleFormState = () => setFormState(prev => !prev);
+
+	return (
+		<chakra.form sx={{ display: "flex", flexDir: "column", gap: "16px" }}>
+			<Heading>Token</Heading>
+			<CustomNumberInput
+				value={tokenAmount}
+				setValue={setTokenAmount}
+				label={"Token Amount"}
+			/>
+			<CustomNumberInput
+				value={weiAmount}
+				setValue={setWeiAmount}
+				label={"Wei Amount"}
+			/>
+			<Checkbox
+				value={allAtOnceChecked}
+				onChange={() => setAllAtOnceChecked(!allAtOnceChecked)}
+			>
+				All At Once
+			</Checkbox>
+			<Button colorScheme="teal" size="lg" type="submit">
+				{formState ? "Buy Token" : "Sell Token"}
+			</Button>
+		</chakra.form>
+	);
+};
+
+export default TokenForm;
