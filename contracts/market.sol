6dacd10c16f560e2136d07bed3f93f8704b4c296
codeparameter <codeparameter.com@gmail.com>
2023-11-03 12:56:21 +0330

    test interface

---
+++ contracts/market.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./NFTMarket.sol";
+import "./dealmaker.sol";
+import "./marketInterface.sol";
+
+contract Market is marketInterface{
+    
+}
