6c2a2772c07681dfbac3058557b027de20a69775
ArshiaZarei <arshiazarei8@gmail.com>
2023-11-03 17:36:22 +0330

    change js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import {
 	createBrowserRouter,
 	createRoutesFromElements
 } from "react-router-dom";
-import RootLayout from "./components/layouts/RootLayout";
-import NFT from "./components/pages/NFT";
+import Root from "./components/layouts/Root";
 import Buy from "./components/pages/Buy";
+import Landing from "./components/pages/Landing";
+import NFTs from "./components/layouts/NFTs";
 import Sell from "./components/pages/Sell";
+import Token from "./components/layouts/Token";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
-		<Route path="/" element={<RootLayout />}>
-			<Route path="nft" element={<NFT />} />
+		<Route path="/" element={<Root />}>
+			<Route index element={<Landing />} />
+			<Route path="token" element={<Token />} />
+			<Route path="nft" element={<NFTs />} />
 			<Route path="buy" element={<Buy />} />
 			<Route path="sell" element={<Sell />} />
 		</Route>
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <ChakraProvider>
-        <App />
-    </ChakraProvider>
+    <React.StrictMode>
+      <ChakraProvider>
+          <App />
+      </ChakraProvider>
+    </React.StrictMode>
 );
