9237857f7aa9c9dcfcdf6dd3fbc34d279704aaea
ArshiaZarei <arshiazarei8@gmail.com>
2023-11-05 22:36:25 +0330

    create grid view for nfts in landing page

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -1,5 +1,30 @@
-const Landing = () => {
-    return (<>landing page</>)
-}
+import { Link } from "react-router-dom";
+import { Box, SimpleGrid, chakra } from "@chakra-ui/react";
+import { useContext } from "react";
+import { nftContext } from "../../context/NftContext";
 
-export default Landing
+const Landing = () => {
+	const { nfts } = useContext(nftContext);
+  console.log(nfts);
+	return (
+		<SimpleGrid minChildWidth="25%" spacingY={12} my={12}>
+			{nfts.map(nft => (
+				<Box
+					as={Link}
+					to={`nft/${nft.id}`}
+					key={nft.id}
+          borderRadius="8px"
+					maxW="300px"
+					aspectRatio="1"
+					display="grid"
+					placeItems="center"
+          overflow="hidden"
+				>
+					<chakra.img objectFit="cover" w="full" h="full" display="block" src={nft.urlObject} />
+				</Box>
+			))}
+		</SimpleGrid>
+	);
+};
+
+export default Landing;
