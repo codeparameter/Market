abd50cb156912a47ab1b50eceef5690e55d675e1
ArshiaZarei <arshiazarei8@gmail.com>
2023-11-05 22:34:33 +0330

    add provider to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
+import NftProvider from "./context/NftContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <ChakraProvider>
-          <App />
+          <NftProvider>
+            <App />
+          </NftProvider>
       </ChakraProvider>
     </React.StrictMode>
 );
