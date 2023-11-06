c03d061520d298ef0a09d53cb2481c384cb95d0e
codeparameter <codeparameter.com@gmail.com>
2023-11-06 14:26:44 +0330

    removing extra contracts js

--- src/js/contracts/abcoin/abcoin.js
+++
@@ -1,7 +0,0 @@
-import { ethers, provider, signer } from "../../ethers";
-import { abi } from "./abi.js";
-
-const address = "0x31B45869B743996E35D26c7f3bbacF1f36b73574";
-const abcoin = new ethers.Contract(address, abi, provider);
-
-export {abcoin};
