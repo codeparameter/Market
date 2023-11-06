5f6fea9a6810e32a908082d64310aeacaebb2bd0
ArshiaZarei <arshiazarei8@gmail.com>
2023-11-06 13:49:42 +0330

    use nft img component in landing page

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { Box, Grid, chakra } from "@chakra-ui/react";
 import { useContext } from "react";
 import { nftContext } from "../../context/NftContext";
+import NftImg from "../common/NftImg";
 
 const Landing = () => {
 	const { nfts } = useContext(nftContext);
@@ -10,22 +11,7 @@ const Landing = () => {
 		<Grid templateColumns="repeat(4, 1fr)" gap={12} my={12}>
 			{nfts.map(nft => (
 				<Link to={`nft/${nft.id}`} key={nft.id}>
-					<Box
-						borderRadius="8px"
-						maxW="300px"
-						aspectRatio="1"
-						display="grid"
-						placeItems="center"
-						overflow="hidden"
-					>
-						<chakra.img
-							objectFit="cover"
-							w="full"
-							h="full"
-							display="block"
-							src={nft.urlObject}
-						/>
-					</Box>
+          <NftImg imgSrc={nft.urlObject} />
 				</Link>
 			))}
 		</Grid>
