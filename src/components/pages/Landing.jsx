7fba61aca4e718e34d4d6b0b5593dd626f1d64c3
ArshiaZarei <arshiazarei8@gmail.com>
2023-11-06 12:29:42 +0330

    extract nft card to its own component

--- src/components/pages/Landing.jsx
+++ src/components/pages/Landing.jsx
@@ -5,23 +5,28 @@ import { nftContext } from "../../context/NftContext";
 
 const Landing = () => {
 	const { nfts } = useContext(nftContext);
-  console.log(nfts);
+	console.log(nfts);
 	return (
 		<Grid templateColumns="repeat(4, 1fr)" gap={12} my={12}>
 			{nfts.map(nft => (
-				<Box
-					as={Link}
-					to={`nft/${nft.id}`}
-					key={nft.id}
-          borderRadius="8px"
-					maxW="300px"
-					aspectRatio="1"
-					display="grid"
-					placeItems="center"
-          overflow="hidden"
-				>
-					<chakra.img objectFit="cover" w="full" h="full" display="block" src={nft.urlObject} />
-				</Box>
+				<Link to={`nft/${nft.id}`} key={nft.id}>
+					<Box
+						borderRadius="8px"
+						maxW="300px"
+						aspectRatio="1"
+						display="grid"
+						placeItems="center"
+						overflow="hidden"
+					>
+						<chakra.img
+							objectFit="cover"
+							w="full"
+							h="full"
+							display="block"
+							src={nft.urlObject}
+						/>
+					</Box>
+				</Link>
 			))}
 		</Grid>
 	);
