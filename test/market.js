fabedc6063de1e2a0fbe42f3956f4a59852b8272
codeparameter <codeparameter.com@gmail.com>
2023-11-07 13:40:04 +0330

    create NFT tested

--- test/market.js
+++ test/market.js
@@ -1,33 +1,41 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-describe('market tests', () => {
+describe("market tests", () => {
+  it("pass abcoin to market construct", async () => {
+    const { market, abcoin } = await setUp();
+    expect(await market.abcoin()).to.equal(abcoin);
+  });
 
-    async function setUp (){
-        const DealMaker = await hre.ethers.getContractFactory('DealMaker');
-        const dealmaker = await DealMaker.deploy();
-        await dealmaker.deployed();
-        const abcoin = await dealmaker.abcoin();
-    
-        const Market = await hre.ethers.getContractFactory('Market');
-        const market = await Market.deploy(abcoin);
-        await market.deployed();
+  it("create NFT", async () => {
+    const { market, otherAccounts } = await setUp();
+    await market.connect(otherAccounts[0]).createNFT("test");
+    const lastId = await market.getLastNFT();
+    expect(await market.showNFT(lastId)).to.equal("test");
+  });
 
-        const [owner, ...otherAccounts] = await ethers.getSigners();
-
-        console.log(owner);
-        console.log(otherAccounts);
-    
-        return {market, abcoin, owner, otherAccounts};
-    }
-
-    it("pass abcoin to market construct", async () => {
-        const {market, abcoin} = await setUp();
-        expect(await market.abcoin()).to.equal(abcoin);
-    });
-
-    // it("auction", async () => {
-    //     const {market, abcoin} = await setUp();
-        
-    //     expect(await market.abcoin()).to.equal(abcoin);
-    // });
+//   it("create Auction", async () => {
+//     const { market, otherAccounts } = await setUp();
+//     await market.connect(otherAccounts[0]).createNFT("test2");
+//     const lastId = await market.getLastNFT();
+//     await market.connect(otherAccounts[0]).createAuction(lastId, 100, 5);
+//     const auction = market.auctions(lastId);
+//     console.log(auction);
+//     expect(await market.showNFT(lastId)).to.equal("test2");
+//   });
 });
+
+async function setUp() {
+  const DealMaker = await hre.ethers.getContractFactory("DealMaker");
+  const dealmaker = await DealMaker.deploy();
+  await dealmaker.deployed();
+
+  const abcoin = await dealmaker.abcoin();
+
+  const Market = await hre.ethers.getContractFactory("Market");
+  const market = await Market.deploy(abcoin);
+  await market.deployed();
+
+  const [owner, ...otherAccounts] = await ethers.getSigners();
+
+  return { market, abcoin, owner, otherAccounts };
+}
